51e3fb26e2f14d5166b45927f920c611fb2e4962
anand raj <git add README.md>
2026-04-30 12:27:18 +0530

    vercelfix1

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,7 +1,7 @@
-// import React, { createContext, useState } from 'react'
-// export const Userdatacontext = createContext()
+import React, { createContext, useState } from 'react'
+export const Userdatacontext = createContext()
 
-// const UserContext = (props) => {
+const UserContext = (props) => {
 //     const [expense, setexpense] = useState();
 //     const [classify, setclassify] = useState();
 //     const [stored, setstored] = useState([]);
@@ -10,37 +10,38 @@
 // const [classify, setclassify] = useState("");
 // const [stored, setstored] = useState([]);
 
-// function addmanually(e) {
-//   e.preventDefault();
+function addmanually(e) {
+  e.preventDefault();
 
-//   if (!expense || !classify) return;
+  if (!expense || !classify) return;
 
-//   const newItem = {
-//     expense: Number(expense),
-//     classify
-//   };
+  const newItem = {
+    expense: Number(expense),
+    classify
+  };
 
-//   setstored(prev => [...prev, newItem]);
+  setstored(prev => [...prev, newItem]);
 
-//   setexpense("");
-//   setclassify("");
-// }
+  setexpense("");
+  setclassify("");
+}
+let a = 1
+  return (
+<Userdatacontext.Provider
+  value={{
+    // expense,
+    // classify,
+    // stored,
+    // setexpense,
+    // setclassify,
+    // addmanually , 
+    a
+  }}
+>
 
-//   return (
-// <Userdatacontext.Provider
-//   value={{
-//     expense,
-//     classify,
-//     stored,
-//     setexpense,
-//     setclassify,
-//     addmanually
-//   }}
-// >
+{props.children}
+   </Userdatacontext.Provider>
+  )
+}
 
-// {props.children}
-//    </Userdatacontext.Provider>
-//   )
-// }
-
-// export default UserContext
+export default UserContext
